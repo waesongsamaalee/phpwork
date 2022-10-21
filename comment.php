604954845fd43524752ccd9f9d8a8e0ecc271097
waesongsamaalee <waesongsamaalee@gmail.com>
2022-10-21 11:50:46 +0700

    ส่งไฟล์ comment ครั้งที่2

---
+++ comment.php
@@ -0,0 +1,14 @@
+<?php
+
+//บรรทัดที่ 6-7 แสดงผลการใช้งาน comment แบบบรรทัดเดียว
+ echo "Waesong sama-alee<br>";
+ echo "Information Technology<br>";
+ //echo "Pattani<br>";
+ #echo "Thailand<br>";
+
+ //บรรทัดที่ 10-12 แสดงผลการใช้งาน comment แบบหลายบรรทัด
+ /*echo "94000<br>";
+ echo "Asia<br>";*/
+ echo "Universal<br>";
+
+?>
