e6c04961561a3e1b96157fc124b654d8f0214676
waesongsamaalee <waesongsamaalee@gmail.com>
2022-10-21 11:28:54 +0700

    ส่งไฟล์ Test ครั้งที่1

--- test.php
+++ test.php
@@ -1,5 +1,22 @@
 <?php
-
     echo "Waesong sama-alee";
-
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST</title>
+</head>
+<body>
+    <br> Infomation Technology <br>
+<?php
+    echo "Pattani";
+?>
+<br>
+</body>
+</html>
+<?php
+    echo "Thailand";
 ?>
